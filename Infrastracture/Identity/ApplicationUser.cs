2255a5f36388aed73eb6685a00b4b25b4ed45312
Reza_Babakhani <Reza_Babakhani@outlook.com>
2020-08-30 14:15:35 +0430

    Commit change ApplicationUser

--- Infrastracture/Identity/ApplicationUser.cs
+++ Infrastracture/Identity/ApplicationUser.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Infrastructure.Identity
 {
    public class ApplicationUser:IdentityUser
     {
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDay { get; set; }
+
+        public bool NewsSubscribe { get; set; }
+
+
+
     }
 }
